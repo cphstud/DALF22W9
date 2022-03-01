c3eac9b2e35136e48b6da080d295036ad4bd45be
thor <thor@cphbusiness.dk>
2022-03-02 00:58:01 +0100

    script to prepare mongo

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash -x
+
+find ./matches/* -type f | while read x
+do
+  grep -l female $x
+done | while read x
+do
+  grep match_id $x
+done | cut  -d\: -f2 |  cut -d\, -f1 | sort | uniq > listoffemgames
+cat listoffemgames  | while  read x
+do
+  cp events/$x.json tmp
+done
+for i in tmp/*json; do sed -i 's/^\[//g' $i; done
+for i in tmp/*json; do sed -i 's/^} \]/}/g' $i; done
+for i in tmp/*json; do sed -i 's/^  \"id/  \"_id/g' $i;done
+for i in tmp/*json; do sed -i 's/^}, {/}\n{/g' $i;done
+#for i in *json;  do mongoimport -d statsbomb -c matches $i;done
+
